560176dae37d408aee68c15b50d5905b6a460fc4
jamari-morrison <RedCater360@yahoo.com>
2024-02-12 13:08:55 -0800

    verify delete functionality

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
    // .mocharc.js
    module.exports = {
     require: 'ts-node/register', // Use ts-node to run TypeScript files
-    spec: 'tests/**/get.test.ts',  // Your test files pattern
+    spec: 'tests/**/delete.test.ts',  // Your test files pattern
     // ... other configurations
   };
--- src/delete.ts
+++ src/delete.ts
@@ -33,6 +33,7 @@ export async function deleteDocument(args: {
   }
 
   let payloadParentId = parentId;
+
   if (websiteUrl) {
     payloadParentId = websiteUrl;
     docId = undefined;
